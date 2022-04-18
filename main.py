9ecf252fcbdec1dc9cf0f94da739037a42187c54
Emmanuel Temitope <emmanueltopea@gmail.com>
2022-04-18 04:03:10 +0100

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import smtplib, ssl
 from getpass import getpass
 
-sender_email = "emmanueltopea@gmail.com"
-receiver_email = "xlfqpnindxett@candassociates.com"
+sender_email = ""
+receiver_email = ""
 sender_password = getpass(prompt='Enter your password: ')
-port = 587
+port = 465 # 465 for SSL and 587 for starttls
 smtp_server = "smtp.gmail.com"
 message = """\
 Subject: New Message
@@ -13,7 +13,7 @@ Hello there."""
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP(smtp_server, port) as server:
-    server.starttls(context=context)
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    # server.starttls(context=context)
     server.login(sender_email, sender_password)
     server.sendmail(sender_email, receiver_email, message)
